9ee9e3532e2d529f7fbad34454269be96a443fa5
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-25 18:11:12 -0300

    feat(app.ts): Rota "/sign-up" de cadastro de usuários criada e configurada

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import UserController from './controllers/UserController';
+import UserModel from './models/UserModel';
+import UserService from './services/UserService';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/sign-up', (req, res, next) => {
+  new UserController(req, res, next).addNewUser(
+    new UserService(new UserModel()),
+  );
+});
 
 export default app;
