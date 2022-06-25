31387afdec61c14a80c8985144cc6336131f23fc
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-25 12:38:23 -0300

    feat(index.ts): Implementação inicial do server express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, ()=> {
+  console.log(`Online na porta ${PORT}`);
+});
+
+export default server;
