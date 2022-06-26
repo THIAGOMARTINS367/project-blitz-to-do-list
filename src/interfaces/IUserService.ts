278b1bfd7667078d9ef1671c188ff9953d1a86d5
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 14:43:59 -0300

    refactor(IUserService.ts): Inclusão de retornos de objetos "error" do tipo "IResponseError"

--- src/interfaces/IUserService.ts
+++ src/interfaces/IUserService.ts
@@ -1,9 +1,10 @@
+import IResponseError from './IResponseError';
 import IUSer from './IUser';
 import IUserData from './IUserData';
 import IUserLogin from './IUserLogin';
 
 interface IUserService {
-  addNewUser(user: IUSer): Promise<Omit<IUserData, 'password'>>,
+  addNewUser(user: IUSer): Promise<Omit<IUserData, 'password'> | IResponseError>,
   userLogin({ email, password }: IUserLogin): Promise<string>,
 }
 
