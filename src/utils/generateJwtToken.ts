1b3e64530788af45eff1af3cf70ef540f6ea6a0d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 17:34:36 -0300

    refactor(generateJwtToken.ts): Refatoração feita para retirar a "super secret", nescessária para gerar tokens JWT, do hardcode

--- src/utils/generateJwtToken.ts
+++ src/utils/generateJwtToken.ts
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken';
 import IUserData from '../interfaces/IUserData';
 
+const superSecretJwt: string = process.env.JWT_SECRET || 'default_password';
+
 const generateJwtToken = (
   validity: string,
   paylod: Omit<IUserData, 'password'>,
-  superSecret: string,
 ): string => {
   const jwtConfig: object = {
     expiresIn: validity,
     algorithm: 'HS256',
   };
-  return jwt.sign({ data: paylod }, superSecret, jwtConfig);
+  return jwt.sign({ data: paylod }, superSecretJwt, jwtConfig);
 };
 
 export default generateJwtToken;
