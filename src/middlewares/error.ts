c2c5b23b1acb75f828c61bd534c9aeffd54dd951
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 14:34:39 -0300

    feat(error.ts): Middleware para captura de erros criado

---
+++ src/middlewares/error.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import IResponseError from '../interfaces/IResponseError';
+
+export default (
+  err: IResponseError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response<{ message: string }> => {
+  const { code, message } = err.error;
+  if (err.error) {
+    return res.status(code).json({ message });
+  }
+  return res.status(500).json({message: 'Internal Server Error !'});
+};
