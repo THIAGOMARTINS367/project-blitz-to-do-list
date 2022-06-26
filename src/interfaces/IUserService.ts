16f1edbb73ed743034144a0b371412f0c88d19ba
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 12:37:55 -0300

    refactor(IUserService.ts): Método "userLogin" criado e tipado na interface

--- src/interfaces/IUserService.ts
+++ src/interfaces/IUserService.ts
@@ -1,8 +1,10 @@
 import IUSer from './IUser';
 import IUserData from './IUserData';
+import IUserLogin from './IUserLogin';
 
 interface IUserService {
-  addNewUser(user: IUSer): Promise<IUserData>,
+  addNewUser(user: IUSer): Promise<Omit<IUserData, 'password'>>,
+  userLogin({ email, password }: IUserLogin): Promise<string>,
 }
 
 export default IUserService;
