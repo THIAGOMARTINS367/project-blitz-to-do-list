dc1a3f1e52453aedb37bcbb7fcb8731095d18c96
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 12:31:35 -0300

    feat(UserService.ts): Método "userLogin", para logar usuário, implementado

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,15 +1,26 @@
 import IUSer from '../interfaces/IUser';
 import IUserData from '../interfaces/IUserData';
+import IUserLogin from '../interfaces/IUserLogin';
 import IUserModel from '../interfaces/IUserModel';
 import IUserService from '../interfaces/IUserService';
 
 class UserService implements IUserService {
   constructor(private model: IUserModel) {}
 
-  async addNewUser(user: IUSer): Promise<IUserData> {
+  async addNewUser(user: IUSer): Promise<Omit<IUserData, 'password'>> {
     const newUser = await this.model.addNewUser(user);
     return newUser;
   }
+
+  async userLogin(body: IUserLogin): Promise<string> {
+    const userExist = await this.model.getUserByEmailAndPassword(body);
+    console.log('userExist:', userExist);
+    if (userExist.length === 1) {
+      return 'Usuário existe !';
+    }
+    return 'Email ou Senha estão incorretos !';
+  }
 }
+
 
 export default UserService;
