9e972090ca10cbe21916b8ab581f5a2f60f91a3c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 12:32:43 -0300

    feat(UserModel.ts): Método "getUserByEmailAndPassword", para validar login do usuário, implementado

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,6 +1,7 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
 import IUSer from '../interfaces/IUser';
 import IUserData from '../interfaces/IUserData';
+import IUserLogin from '../interfaces/IUserLogin';
 import IUserModel from '../interfaces/IUserModel';
 import connection from './connection';
 
@@ -13,22 +14,55 @@ class UserModel implements IUserModel {
     }
   }
 
-  async addNewUser({ admin, firstName, lastName, email, password }: IUSer): Promise<IUserData> {
-    const [rows] = await this.connectionDb.execute(`
+  async addNewUser({
+    admin,
+    firstName,
+    lastName,
+    email,
+    password,
+  }: IUSer): Promise<Omit<IUserData, 'password'>> {
+    const [rows] = await this.connectionDb.execute(
+      `
     INSERT INTO blitz_toDoList.user
       (admin, first_name, last_name, email, password)
     VALUES
-      (?,?,?,?,?)`,[admin, firstName, lastName, email, password],
+      (?,?,?,?,?)`,
+      [admin, firstName, lastName, email, password]
     );
     const { insertId } = rows as ResultSetHeader;
+    if (admin === 1) {
+      admin = true;
+    } else {
+      admin = false;
+    }
     return {
       userId: insertId,
       admin,
       firstName,
       lastName,
       email,
-      password
+    };
+  }
+
+  async getUserByEmailAndPassword({
+    email,
+    password,
+  }: IUserLogin): Promise<Omit<IUserData[], 'password'>> {
+    const [rows] = await this.connectionDb.execute(`
+    SELECT
+      user_id, admin, first_name, last_name, email
+    FROM
+      blitz_toDoList.user
+    WHERE email = ? AND password = ?`,
+      [email, password]
+    );
+    const userData = rows as Omit<IUserData[], 'password'>
+    if (userData[0].admin === 1) {
+      userData[0].admin = true;
+    } else {
+      userData[0].admin = false;
     }
+    return userData;
   }
 }
 
