490ff7e32d6e3e6b5318efa77db7c0724d80e239
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 12:30:18 -0300

    feat(UserController.ts): Método "userLogin", para logar usuário, implementado

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import IUSer from '../interfaces/IUser';
+import IUserLogin from '../interfaces/IUserLogin';
 import IUserService from '../interfaces/IUserService';
 
 class UserController {
@@ -13,6 +14,12 @@ class UserController {
     const body: IUSer = this.req.body;
     const result = await service.addNewUser(body);
     this.res.status(201).json(result);
+  }
+
+  async userLogin(service: IUserService) {
+    const body: IUserLogin = this.req.body;
+    const result = await service.userLogin(body);
+    this.res.status(200).json(result);
   }
 }
 
