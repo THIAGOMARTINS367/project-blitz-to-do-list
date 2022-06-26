6adceef37422e2eb84e33ec67b2226d110401b70
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 12:37:15 -0300

    refactor(IUserModel.ts): Método "getUserByEmailAndPassword" criado e tipado na interface

--- src/interfaces/IUserModel.ts
+++ src/interfaces/IUserModel.ts
@@ -1,5 +1,9 @@
+import IUserData from './IUserData';
+import IUserLogin from './IUserLogin';
 import IUserService from './IUserService';
 
-interface IUserModel extends IUserService {};
+interface IUserModel extends Omit<IUserService, 'userLogin'> {
+  getUserByEmailAndPassword({ email, password }: IUserLogin): Promise<Omit<IUserData[], 'password'>>,
+};
 
 export default IUserModel;
