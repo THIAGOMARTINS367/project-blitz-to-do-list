7ed5d35adf72be7d0fe06ee312e69d53eab6bd2e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-26 21:51:08 -0300

    feat(TaskModel.ts): Model "TaskModel" e métodos "getUserTaskList" e "serialize", para obter lista de tarefas de um usuário e formartar chaves dos objetos retornados do BD respectivamente, implementados

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,45 @@
+import { Pool } from 'mysql2/promise';
+import ITask from '../interfaces/ITask';
+import ITaskDb from '../interfaces/ITaskDb';
+import ITaskModel from '../interfaces/ITaskModel';
+import IUserData from '../interfaces/IUserData';
+import connection from './connection';
+
+class TaskModel implements ITaskModel {
+  protected connectionDb: Pool = connection;
+
+  constructor(connectionDb?: Pool, private taskList: ITaskDb[] | [] = []) {
+    if (connectionDb) {
+      this.connectionDb = connectionDb;
+    }
+  }
+
+  serialize(): Omit<ITask[], 'userId'> {
+    const taskListFormatted = this.taskList.map(({
+      task_id,
+      task_content,
+      status,
+      created_at,
+      updated_at,
+    }) => ({
+      taskId: task_id,
+      taskContent: task_content,
+      status,
+      createdAt: created_at,
+      updatedAt: updated_at,
+    }));
+    return taskListFormatted as Omit<ITask[], 'userId'>;
+  }
+
+  async getUserTaskList({ userId }: IUserData): Promise<Omit<ITask[], 'userId'>> {
+    const [rows] = await this.connectionDb.execute(
+      'SELECT * FROM blitz_toDoList.task WHERE user_id = ?',
+      [userId],
+    );
+    this.taskList = rows as ITaskDb[];
+    const taskListFormatted = this.serialize();
+    return taskListFormatted as Omit<ITask[], 'userId'>;
+  }
+}
+
+export default TaskModel;
