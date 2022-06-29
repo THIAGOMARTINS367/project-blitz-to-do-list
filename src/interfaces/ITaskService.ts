a5d4004b96dff002a180c789ddd6d3ab580ef4b7
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 16:20:22 -0300

    feat(ITaskService.ts): Método "addNewTask" criado e tipado na interface

--- src/interfaces/ITaskService.ts
+++ src/interfaces/ITaskService.ts
@@ -3,6 +3,7 @@ import IUserData from './IUserData';
 
 interface ITaskService {
   getUserTaskList(userData: IUserData): Promise<Omit<ITask[], 'userId'>>,
+  addNewTask(userData: IUserData, body: ITask[]): Promise<{ message: string }>,
 }
 
 export default ITaskService;
