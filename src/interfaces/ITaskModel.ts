165e626a0b1b82ff62ea59eb8edf638750592a8f
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 16:20:49 -0300

    feat(ITaskModel.ts): Método "addNewTask" criado e tipado na interface

--- src/interfaces/ITaskModel.ts
+++ src/interfaces/ITaskModel.ts
@@ -1,8 +1,12 @@
 import ITask from './ITask';
 import ITaskService from './ITaskService';
 
-interface ITaskModel extends ITaskService {
-  serialize(): Omit<ITask[], 'userId'>,
+interface ITaskModel extends Omit<ITaskService, 'addNewTask'> {
+  serialize(): Omit<ITask[], 'userId'>;
+  addNewTask(
+    tasksData: (string | number)[],
+    queryInjection: string[]
+  ): Promise<{ message: string }>;
 }
 
 export default ITaskModel;
