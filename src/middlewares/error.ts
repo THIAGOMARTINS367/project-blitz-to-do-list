96860a8ae08be4853f29f3614c0e5d550ecc4abd
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 20:08:06 -0300

    refactor(error.ts): Middleware refatorado para capturar erros que podem quebrar a aplicação

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -7,9 +7,19 @@ export default (
   res: Response,
   _next: NextFunction,
 ): Response<{ message: string }> => {
-  const { code, message } = err.error;
-  if (err.error) {
-    return res.status(code).json({ message });
+  try {
+    if (!err.error) {
+      err.error = { code: 500, message: 'Internal Server Error!' };
+      console.error('Error1 - Error in "error middleware" !');
+    }
+    const { code, message } = err.error;
+    if (err.error) {
+      return res.status(code).json({ message });
+    }
+    console.error('Error2 - Error in "error middleware" !');
+    return res.status(500).json({ message: 'Internal Server Error !' });
+  } catch (error) {
+    console.error('Error3 - Error in "error middleware" !');
+    return res.status(500).json({ message: 'Internal Server Error !' });
   }
-  return res.status(500).json({ message: 'Internal Server Error !' });
 };
