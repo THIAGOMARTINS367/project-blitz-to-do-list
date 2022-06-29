2900ed909a3efe83a0b877b643b30c188297b87e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 18:42:08 -0300

    refactor(ITaskService.ts): Inserção do tipo "IResponseError" no retorno do método "updateTask" da interface

--- src/interfaces/ITaskService.ts
+++ src/interfaces/ITaskService.ts
@@ -5,7 +5,7 @@ import IUserData from './IUserData';
 interface ITaskService {
   getUserTaskList(userData: IUserData): Promise<Omit<ITask[], 'userId'>>,
   addNewTask(userData: IUserData, body: ITask[]): Promise<{ message: string } | IResponseError>,
-  updateTask(userData: IUserData, taskId: number, body: ITask): Promise<{ message: string }>,
+  updateTask(userData: IUserData, taskId: number, body: ITask): Promise<{ message: string } | IResponseError>,
 }
 
 export default ITaskService;
