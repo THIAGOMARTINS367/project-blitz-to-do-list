271a27daf0e80a8f7c1696ba023fcb2980ecb55d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 16:18:35 -0300

    feat(TaskModel.ts): Método "addNewTask", para adicionar novas tarefas de um determinado usuário, criado e implementado

--- src/models/TaskModel.ts
+++ src/models/TaskModel.ts
@@ -15,23 +15,21 @@ class TaskModel implements ITaskModel {
   }
 
   serialize(): Omit<ITask[], 'userId'> {
-    const taskListFormatted = this.taskList.map(({
-      task_id,
-      task_content,
-      status,
-      created_at,
-      updated_at,
-    }) => ({
-      taskId: task_id,
-      taskContent: task_content,
-      status,
-      createdAt: created_at,
-      updatedAt: updated_at,
-    }));
+    const taskListFormatted = this.taskList.map(
+      ({ task_id, task_content, status, created_at, updated_at }) => ({
+        taskId: task_id,
+        taskContent: task_content,
+        status,
+        createdAt: created_at,
+        updatedAt: updated_at,
+      }),
+    );
     return taskListFormatted as Omit<ITask[], 'userId'>;
   }
 
-  async getUserTaskList({ userId }: IUserData): Promise<Omit<ITask[], 'userId'>> {
+  async getUserTaskList({
+    userId,
+  }: IUserData): Promise<Omit<ITask[], 'userId'>> {
     const [rows] = await this.connectionDb.execute(
       'SELECT * FROM blitz_toDoList.task WHERE user_id = ?',
       [userId],
@@ -39,6 +37,21 @@ class TaskModel implements ITaskModel {
     this.taskList = rows as ITaskDb[];
     const taskListFormatted = this.serialize();
     return taskListFormatted as Omit<ITask[], 'userId'>;
+  }
+
+  async addNewTask(
+    tasksData: (string | number)[],
+    queryInjection: string[],
+  ): Promise<{ message: string }> {
+    const [rows] = await this.connectionDb.execute(
+      `INSERT INTO
+        blitz_toDoList.task (task_content, status, user_id)
+      VALUES
+        ${queryInjection.join(',')}`,
+      [...tasksData],
+    );
+    this.taskList = rows as ITaskDb[];
+    return { message: 'Tasks saved successfully!' };
   }
 }
 
