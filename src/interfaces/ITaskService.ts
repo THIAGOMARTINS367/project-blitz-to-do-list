b0a5cf8fc3b9beaf7ee8b1053543cc30e76d5bba
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-06-29 20:56:53 -0300

    refactor(ITaskService.ts): Alteração do tipo de retorno do método "updateTask" para possibilitar que ele retorne a tarefa recém-atualizada

--- src/interfaces/ITaskService.ts
+++ src/interfaces/ITaskService.ts
@@ -5,7 +5,7 @@ import IUserData from './IUserData';
 interface ITaskService {
   getUserTaskList(userData: IUserData): Promise<Omit<ITask[], 'userId'> | IResponseError>,
   addNewTask(userData: IUserData, body: ITask[]): Promise<{ message: string } | IResponseError>,
-  updateTask(userData: IUserData, taskId: number, body: ITask): Promise<{ message: string } | IResponseError>,
+  updateTask(userData: IUserData, taskId: number, body: ITask): Promise<ITask | IResponseError>,
 }
 
 export default ITaskService;
