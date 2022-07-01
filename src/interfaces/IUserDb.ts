9dc04e8bfe1f284746844efcda8675fd66e54e0a
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-01 19:44:08 -0300

    refactor(IUserDb.ts): Omissão do atributo "password" na interface estendida

--- src/interfaces/IUserDb.ts
+++ src/interfaces/IUserDb.ts
@@ -1,6 +1,6 @@
 import IUser from './IUser';
 
-interface IUSerDb extends Omit<IUser, 'userId' | 'fisrtName' | 'lastName'> {
+interface IUSerDb extends Omit<IUser, 'userId' | 'fisrtName' | 'lastName' | 'password'> {
   user_id: number,
   first_name: string,
   last_name: string,
