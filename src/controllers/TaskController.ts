34dad227286f9110d986a7a9b44e2c96ca10d378
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-02 13:06:19 -0300

    refactor(TaskController.ts): Refatoração feita para aumentar tipagem no código

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
+import IResponseError from '../interfaces/IResponseError';
+import ITask from '../interfaces/ITask';
 import ITaskService from '../interfaces/ITaskService';
+import IUserData from '../interfaces/IUserData';
 
 class TaskController {
   constructor(
@@ -8,40 +11,53 @@ class TaskController {
     private next: NextFunction,
   ) {}
 
-  async getUserTaskList(service: ITaskService) {
-    const { userData } = this.req;
-    const result = await service.getUserTaskList(userData);
+  async getUserTaskList(service: ITaskService):
+  Promise<void | Response<Omit<ITask[], 'userId'> | IResponseError>> {
+    const user: IUserData = this.req.userData;
+    const result:
+    Omit<ITask[], 'userId'> | IResponseError = await service.getUserTaskList(user);
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(200).json(result);
+    return this.res.status(200).json(result);
   }
 
-  async addNewTask(service: ITaskService) {
-    const { userData, body } = this.req;
-    const result = await service.addNewTask(userData, body);
+  async addNewTask(service: ITaskService):
+  Promise<void | Response<IResponseError | { message: string }>> {
+    const user: IUserData = this.req.userData;
+    const requestBody: ITask[] = this.req.body;
+    const result: IResponseError | {
+      message: string,
+    } = await service.addNewTask(user, requestBody);
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(201).json(result);
+    return this.res.status(201).json(result);
   }
 
-  async updateTask(service: ITaskService) {
-    const { userData, body, params: { taskId } } = this.req;
-    const result = await service.updateTask(userData, Number(taskId), body);
+  async updateTask(service: ITaskService):
+  Promise<void | Response<ITask | IResponseError>> {
+    const user: IUserData = this.req.userData;
+    const requestBody: ITask = this.req.body;
+    const id: string = this.req.params.taskId;
+    const result:
+    ITask | IResponseError = await service.updateTask(user, Number(id), requestBody);
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(200).json(result);
+    return this.res.status(200).json(result);
   }
 
-  async deleteTasks(service: ITaskService) {
-    const { userData, body } = this.req;
-    const result = await service.deleteTasks(userData, body);
+  async deleteTasks(service: ITaskService):
+  Promise<void | Response<IResponseError | number[]>> {
+    const user: IUserData = this.req.userData;
+    const requestBody: { tasks: number[] } = this.req.body;
+    const result:
+    IResponseError | number[] = await service.deleteTasks(user, requestBody);
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(200).json({ deletedTasks: result });
+    return this.res.status(200).json({ deletedTasks: result });
   }
 }
 
