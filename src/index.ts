e57b4ae6ee54d29ef99bdb2baa7714b0f4b5fb15
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-02 17:49:39 -0300

    refactor: Limpeza geral na branch "main", pois back-end e front-end serão feitos no mesmo repositório

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, ()=> {
-  console.log(`Online na porta ${PORT}`);
-});
-
-export default server;
