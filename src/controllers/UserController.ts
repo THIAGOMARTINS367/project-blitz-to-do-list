3bf160131b919b86d42a1cfcb75e79d0df293e90
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-02 13:39:57 -0300

    refactor(UserController.ts): Refatoração feita para aumentar tipagem no código

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import IResponseError from '../interfaces/IResponseError';
+import IToken from '../interfaces/IToken';
 import IUser from '../interfaces/IUser';
 import IUserLogin from '../interfaces/IUserLogin';
 import IUserService from '../interfaces/IUserService';
+import IUserToken from '../interfaces/IUserToken';
 
 class UserController {
   constructor(
@@ -10,24 +13,24 @@ class UserController {
     private next: NextFunction,
   ) {}
 
-  async addNewUser(service: IUserService) {
-    const { body } = this.req;
-    const user: IUser = body;
-    const result = await service.addNewUser(user);
+  async addNewUser(service: IUserService):
+  Promise<void | Response<IResponseError | IUserToken>> {
+    const newUser: IUser = this.req.body;
+    const result = await service.addNewUser(newUser) as IUserToken | IResponseError;
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(201).json(result);
+    return this.res.status(201).json(result);
   }
 
-  async userLogin(service: IUserService) {
-    const { body } = this.req;
-    const user: IUserLogin = body;
-    const result = await service.userLogin(user);
+  async userLogin(service: IUserService):
+  Promise<void | Response<IResponseError | IToken>> {
+    const user: IUserLogin = this.req.body;
+    const result: IResponseError | IToken = await service.userLogin(user);
     if (Object.keys(result).includes('error')) {
       return this.next(result);
     }
-    this.res.status(200).json(result);
+    return this.res.status(200).json(result);
   }
 }
 
