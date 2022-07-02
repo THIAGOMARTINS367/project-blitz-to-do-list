fbef5b166566330e6aa0cb2c01a80a6e2a57533f
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-02 17:34:28 -0300

    refactor(TaskModel.ts): Refatoração feita para implementar blocos try/catch para controlar possíves erros de requisição ao BD

--- src/models/TaskModel.ts
+++ src/models/TaskModel.ts
@@ -12,32 +12,40 @@ class TaskModel implements ITaskModel {
   async getUserTaskList({
     userId,
   }: IUserData): Promise<Omit<ITask[], 'userId'>> {
-    const [rows] = await this.connectionDb.execute(
-      `SELECT
-        task_id AS taskId,
-        task_content AS taskContent,
-        status,
-        created_at AS createdAt,
-        updated_at AS updatedAt
-      FROM blitz_toDoList.task WHERE user_id = ?`,
-      [userId],
-    );
-    const taskList = rows as Omit<ITask[], 'userId'>;
-    return taskList;
+    try {
+      const [rows] = await this.connectionDb.execute(
+        `SELECT
+          task_id AS taskId,
+          task_content AS taskContent,
+          status,
+          created_at AS createdAt,
+          updated_at AS updatedAt
+        FROM blitz_toDoList.task WHERE user_id = ?`,
+        [userId],
+      );
+      const taskList = rows as Omit<ITask[], 'userId'>;
+      return taskList;
+    } catch (error) {
+      throw new Error('');
+    }
   }
 
   async addNewTask(
     tasksData: (string | number)[],
     queryInjection: string[],
   ): Promise<{ message: string }> {
-    await this.connectionDb.execute(
-      `INSERT INTO
-        blitz_toDoList.task (task_content, status, user_id)
-      VALUES
-        ${queryInjection.join(',')}`,
-      [...tasksData],
-    );
-    return { message: 'Tasks saved successfully!' };
+    try {
+      await this.connectionDb.execute(
+        `INSERT INTO
+          blitz_toDoList.task (task_content, status, user_id)
+        VALUES
+          ${queryInjection.join(',')}`,
+        [...tasksData],
+      );
+      return { message: 'Tasks saved successfully!' };
+    } catch (error) {
+      throw new Error('');
+    }
   }
 
   async updateTask(
@@ -45,15 +53,19 @@ class TaskModel implements ITaskModel {
     taskId: number,
     { taskContent, status, createdAt }: ITask,
   ): Promise<ITask> {
-    const updatedAt: Date = new Date();
-    await this.connectionDb.execute(
-      `UPDATE
-        blitz_toDoList.task
-      SET task_content = ?, status = ?, updated_at = ?
-      WHERE task_id = ? AND user_id = ?`,
-      [taskContent, status, updatedAt, taskId, userId],
-    );
-    return { taskId, taskContent, status, createdAt, updatedAt } as ITask;
+    try {
+      const updatedAt: Date = new Date();
+      await this.connectionDb.execute(
+        `UPDATE
+          blitz_toDoList.task
+        SET task_content = ?, status = ?, updated_at = ?
+        WHERE task_id = ? AND user_id = ?`,
+        [taskContent, status, updatedAt, taskId, userId],
+      );
+      return { taskId, taskContent, status, createdAt, updatedAt } as ITask;
+    } catch (error) {
+      throw new Error('');
+    }
   }
 
   async getUserTaskById(
@@ -61,35 +73,39 @@ class TaskModel implements ITaskModel {
     taskIds: number[],
     mysqlInjection: string[],
   ): Promise<ITask[]> {
-    const [rows] = await this.connectionDb.execute(
-      `SELECT
-        task_id AS taskId,
-        task_content AS taskContent,
-        status,
-        created_at AS createdAt,
-        updated_at AS updatedAt
-      FROM
-        blitz_toDoList.task
-      WHERE user_id = ? AND task_id IN(${mysqlInjection.join(',')})`,
-      [userId, ...taskIds],
-    );
-    const taskList = rows as Omit<ITask[], 'userId'>;
-    return taskList;
+    try {
+      const [rows] = await this.connectionDb.execute(
+        `SELECT task_id AS taskId, task_content AS taskContent, status,
+          created_at AS createdAt,
+          updated_at AS updatedAt
+        FROM blitz_toDoList.task
+        WHERE user_id = ? AND task_id IN(${mysqlInjection.join(',')})`,
+        [userId, ...taskIds],
+      );
+      const taskList = rows as Omit<ITask[], 'userId'>;
+      return taskList;
+    } catch (error) {
+      throw new Error('');
+    }
   }
 
   async getUserById(userId: number): Promise<IUserData[]> {
-    const [rows] = await this.connectionDb.execute(
-      `SELECT
-        user_id AS userId,
-        admin,
-        first_name AS firstName,
-        last_name AS lastName,
-        email
-      FROM blitz_toDoList.user WHERE user_id = ?`,
-      [userId],
-    );
-    const userData = rows as IUserData[];
-    return userData;
+    try {
+      const [rows] = await this.connectionDb.execute(
+        `SELECT
+          user_id AS userId,
+          admin,
+          first_name AS firstName,
+          last_name AS lastName,
+          email
+        FROM blitz_toDoList.user WHERE user_id = ?`,
+        [userId],
+      );
+      const userData = rows as IUserData[];
+      return userData;
+    } catch (error) {
+      throw new Error('');
+    }
   }
 
   async deleteTasks(
@@ -97,13 +113,17 @@ class TaskModel implements ITaskModel {
     body: number[],
     queryInjection: string[],
   ): Promise<number[]> {
-    await this.connectionDb.execute(
-      `DELETE FROM
-        blitz_toDoList.task
-      WHERE user_id = ? AND task_id IN(${queryInjection.join(',')})`,
-      [userId, ...body],
-    );
-    return body;
+    try {
+      await this.connectionDb.execute(
+        `DELETE FROM
+          blitz_toDoList.task
+        WHERE user_id = ? AND task_id IN(${queryInjection.join(',')})`,
+        [userId, ...body],
+      );
+      return body;
+    } catch (error) {
+      throw new Error('');
+    }
   }
 }
 
