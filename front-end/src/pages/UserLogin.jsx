15cac77f6df7fe7fa519c5c601d3302960da2c7b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 12:43:15 -0300

    feat(UserLogin.jsx): Página "UserLogin" criada e implementação inicial feita

---
+++ front-end/src/pages/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserLogin() {
+  return(
+    <section>
+      <span>Essa é a página de Login do usuário</span>
+    </section>
+  );
+}
+
+export default UserLogin;
