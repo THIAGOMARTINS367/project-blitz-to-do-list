596a25e784f5a17bc0dc9c61e2cc27820f6d769c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 20:18:58 -0300

    refactor(UserLogin.jsx): Implementação da página de login do usuário

--- front-end/src/pages/UserLogin.jsx
+++ front-end/src/pages/UserLogin.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
+import sendUserData from '../services/sendUserData';
+import LabelAndInput from '../components/LabelAndInput';
 
 function UserLogin() {
+  const {
+    stateLogin,
+    setStateLogin,
+    stateLoginResponse,
+    setStateLoginResponse,
+  } = useContext(Context);
   return (
     <section>
-      <span>Essa é a página de Login do usuário</span>
+      <h1>LOGIN</h1>
+      <form>
+        <LabelAndInput
+          labelContent="Email"
+          inputId="input3"
+          inputType="email"
+          onChangeEvent={({
+            target,
+          }) => setStateLogin({ ...stateLogin, email: target.value })}
+        />
+
+        <br />
+        <br />
+
+        <LabelAndInput
+          labelContent="Senha"
+          inputId="input4"
+          inputType="password"
+          onChangeEvent={({
+            target,
+          }) => setStateLogin({ ...stateLogin, password: target.value })}
+        />
+      </form>
+      {
+        Object.keys(stateLoginResponse).includes('message')
+          ? <div>{stateLoginResponse.message}</div> : false
+      }
+
+      <br />
+
+      <button
+        type="button"
+        onClick={async () => {
+          setStateLoginResponse(await sendUserData(stateLogin, 'http://localhost:3001/login'));
+        }}
+      >
+        Login
+      </button>
     </section>
   );
 }
