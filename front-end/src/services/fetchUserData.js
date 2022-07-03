0a61d864e3e239058fc7e31192faa887b897d97c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 17:08:10 -0300

    feat(fetchUserData.js): Criação e implementação da função que envia dados de casdastro do usuário para o back-end

---
+++ front-end/src/services/fetchUserData.js
@@ -0,0 +1,16 @@
+const fetchUserData = async (state) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    },
+    body: JSON.stringify(state),
+  };
+  fetch('http://localhost:3001/sign-up', options)
+    .then((data) => data.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log('ERROR:', error));
+};
+
+export default fetchUserData;
