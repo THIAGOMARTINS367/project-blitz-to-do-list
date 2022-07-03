340564f5d8565732ab9638d9c5574eb4b9c88d3b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 17:02:30 -0300

    feat(App.jsx): Implementação do "ContextProvider" no "App" React

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,15 +4,18 @@ import { Route, Switch } from 'react-router-dom';
 import UserSignUp from './pages/UserSignUp';
 import UserLogin from './pages/UserLogin';
 import ToDoList from './pages/ToDoList';
+import ContextProvider from './context/ContextProvider';
 
 function App() {
   return (
     <main>
-      <Switch>
-        <Route path="/to-do-list/sign-up" component={UserSignUp} />
-        <Route path="/to-do-list/login" render={UserLogin} />
-        <Route path="/" render={ToDoList} />
-      </Switch>
+      <ContextProvider>
+        <Switch>
+          <Route path="/to-do-list/sign-up" component={UserSignUp} />
+          <Route path="/to-do-list/login" render={UserLogin} />
+          <Route path="/" render={ToDoList} />
+        </Switch>
+      </ContextProvider>
     </main>
   );
 }
