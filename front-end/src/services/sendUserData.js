47891211673a3d34e76d02e19be5cfaf34f24b95
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 17:45:13 -0300

    refactor(sendUserData.js): Refatoração feita na lógica da requisição POST

--- front-end/src/services/sendUserData.js
+++ front-end/src/services/sendUserData.js
@@ -1,16 +1,16 @@
-const sendUserData = async (state) => {
+const sendUserData = async (bodyData) => {
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-cache',
     },
-    body: JSON.stringify(state),
+    body: JSON.stringify(bodyData),
   };
-  fetch('http://localhost:3001/sign-up', options)
-    .then((data) => data.json())
-    .then((data) => console.log(data))
+  const response = await fetch('http://localhost:3001/sign-up', options)
     .catch((error) => console.log('ERROR:', error));
+  const data = await response.json();
+  return data;
 };
 
 export default sendUserData;
