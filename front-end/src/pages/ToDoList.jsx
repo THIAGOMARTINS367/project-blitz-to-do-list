2e783362a65b8593d40848f1633e5e986ec83dc4
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 12:42:43 -0300

    feat(ToDoList.jsx): Página "ToDoList" criada e implementação inicial feita

---
+++ front-end/src/pages/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToDoList() {
+  return(
+    <section>
+      <span>Essa é a página da Lista de Tarefas</span>
+    </section>
+  );
+}
+
+export default ToDoList;
