2ae75728f0b62d8fe657249faacf06d408fd6907
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 12:43:47 -0300

    feat(UserSignUp.jsx): Página "UserSignUp" criada e implementação inicial feita

---
+++ front-end/src/pages/UserSignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserSignUp() {
+  return(
+    <section>
+      <span>Essa é a página de Cadastro do Usuário</span>
+    </section>
+  );
+}
+
+export default UserSignUp;
