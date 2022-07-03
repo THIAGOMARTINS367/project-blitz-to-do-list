1c9e372ab74b713deb738daa194017c75aa98098
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-03 17:03:33 -0300

    feat(UserSignUp.jsx): Implementação do formulário de cadastro do usuário

--- front-end/src/pages/UserSignUp.jsx
+++ front-end/src/pages/UserSignUp.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
+import fetchUserData from '../services/fetchUserData';
 
 function UserSignUp() {
-  return(
+  const { state, setState } = useContext(Context);
+  return (
     <section>
-      <span>Essa é a página de Cadastro do Usuário</span>
+      <h1>Sign-Up</h1>
+      <form>
+        <label htmlFor="input1">
+          NOME
+          <br />
+          <input type="text" id="input1" onChange={({ target }) => setState({ ...state, firstName: target.value })} />
+        </label>
+
+        <br />
+        <br />
+
+        <label htmlFor="input2">
+          SOBRENOME
+          <br />
+          <input type="text" id="input2" onChange={({ target }) => setState({ ...state, lastName: target.value })} />
+        </label>
+
+        <br />
+        <br />
+
+        <label htmlFor="input3">
+          EMAIL
+          <br />
+          <input type="text" id="input3" onChange={({ target }) => setState({ ...state, email: target.value })} />
+        </label>
+
+        <br />
+        <br />
+
+        <label htmlFor="input4">
+          SENHA
+          <br />
+          <input type="password" id="input4" onChange={({ target }) => setState({ ...state, password: target.value })} />
+        </label>
+
+        <br />
+        <br />
+
+        <label htmlFor="select-admin">
+          VOCÊ É UM ADMINISTRADOR?
+          <br />
+          <select id="select-admin" onChange={({ target }) => setState({ ...state, admin: target.value })}>
+            <option value="false">NÃO</option>
+            <option value="true">SIM</option>
+          </select>
+        </label>
+
+        <br />
+        <br />
+
+        <button type="button" onClick={() => fetchUserData(state)}>CADASTRAR</button>
+      </form>
     </section>
   );
 }
