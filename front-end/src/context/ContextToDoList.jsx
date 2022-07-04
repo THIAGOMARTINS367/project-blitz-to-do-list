1550cbe64a0034818b5489466e71328c7cebcd54
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-04 16:34:04 -0300

    refactor(ContextToDoList.jsx): Refatoração feita para implementar a lógica de sessão do usuário restrigindo acesso a lista de tarefas a somente usuários logados

--- front-end/src/context/ContextToDoList.jsx
+++ front-end/src/context/ContextToDoList.jsx
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import fetchToDoList from '../services/fetchToDoList';
 
 function ContextToDoList() {
   const [stateToDoList, setToDoList] = useState([]);
   const [stateToDoListResponse, setStateToDoListResponse] = useState([]);
+  const [stateUserToken, setStateUserToken] = useState(undefined);
+  const history = useHistory();
 
   useEffect(() => {
-    fetchToDoList('http://localhost:3001/to-do-list', '')
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setToDoList(data);
-        }
-      });
-  }, []);
+    const { location: { pathname } } = history;
+    const token = stateUserToken || localStorage.getItem('userToken');
+    if (pathname === '/to-do-list' && token && token.length > 0) {
+      fetchToDoList('http://localhost:3001/to-do-list', token)
+        .then((data) => {
+          console.log('data:', data);
+          if (Array.isArray(data)) {
+            setToDoList(data);
+          } else {
+            localStorage.setItem('userToken', undefined);
+            history.push('/to-do-list/login');
+          }
+        });
+    } else if (pathname === '/to-do-list') {
+      history.push('/to-do-list/login');
+    }
+  }, [stateUserToken]);
 
   return {
     stateToDoList,
     setToDoList,
     stateToDoListResponse,
     setStateToDoListResponse,
+    stateUserToken,
+    setStateUserToken,
   };
 }
 
