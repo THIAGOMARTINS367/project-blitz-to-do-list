e3da7572472e30cfd1755d644ac5043546eb0699
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-04 12:39:41 -0300

    refactor(ToDoList.jsx): Implementação da lista de tarefas

--- front-end/src/pages/ToDoList.jsx
+++ front-end/src/pages/ToDoList.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 function ToDoList() {
+  const {
+    stateToDoList,
+  } = useContext(Context);
   return (
     <section>
-      <span>Essa é a página da Lista de Tarefas</span>
+      <h1>LISTA DE TAREFAS</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>Status</th>
+            <th>Última Alteração</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            stateToDoList.map(({ taskContent, status, updatedAt }) => (
+              <tr>
+                <td>{taskContent}</td>
+                <td>{status}</td>
+                <td>{updatedAt}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </section>
   );
 }
