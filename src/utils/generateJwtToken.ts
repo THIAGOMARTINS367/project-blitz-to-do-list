95b0da0352c73ca18b9d98f5c990f7a37fa602a9
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-04 17:01:05 -0300

    refactor(generateJwtToken.ts): Refatoração feita para tornar a validade do token do usuário uma variável de ambiente

--- src/utils/generateJwtToken.ts
+++ src/utils/generateJwtToken.ts
@@ -1,13 +1,12 @@
 import jwt from 'jsonwebtoken';
-import superSecretJwt from '../contans';
+import superSecretJwt, { tokenValidity } from '../contans';
 import IUserData from '../interfaces/IUserData';
 
 const generateJwtToken = (
-  validity: string,
   paylod: IUserData,
 ): string => {
   const jwtConfig: object = {
-    expiresIn: validity,
+    expiresIn: tokenValidity,
     algorithm: 'HS256',
   };
   return jwt.sign({ data: paylod }, superSecretJwt, jwtConfig);
