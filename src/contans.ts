e75dc7a396bb1c316cf13318729740a9e2fd2c32
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-04 16:59:52 -0300

    feat(constants.ts): Constante "tokenValidity" criada

--- src/contans.ts
+++ src/contans.ts
@@ -1,3 +1,4 @@
 const superSecretJwt: string = process.env.JWT_SECRET || 'default_password';
+export const tokenValidity: string = process.env.USER_TOKEN_VALIDITY || '24h';
 
 export default superSecretJwt;
