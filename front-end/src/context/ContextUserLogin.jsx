2cf55fb947d60ee1c751d39238bb1a5865365589
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-04 16:36:20 -0300

    feat(ContextUserLogin.jsx): Implementação da lógica de sessão do usuário possibilitando que caso o token salvo do usuário ainda seja válido não será nescessário um novo login

--- front-end/src/context/ContextUserLogin.jsx
+++ front-end/src/context/ContextUserLogin.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function ContextUserLogin() {
   const [stateLogin, setStateLogin] = useState({});
   const [stateLoginResponse, setStateLoginResponse] = useState({});
+  const history = useHistory();
+
+  useEffect(() => {
+    const userToken = localStorage.getItem('userToken');
+    if (userToken && userToken.length > 0) {
+      history.push('/to-do-list');
+    }
+  }, []);
 
   return {
     stateLogin,
